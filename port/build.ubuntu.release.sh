8faa389456f3c3d5aa5a2d7b983b72a61faaa9b2
Grigore Stefan <g_stefan@yahoo.com>
2021-01-03 04:01:34 +0200

    release build

--- port/build.ubuntu.release.sh
+++ port/build.ubuntu.release.sh
@@ -3,11 +3,11 @@
 # http://unlicense.org/
 # Created by Grigore Stefan <g_stefan@yahoo.com>
 
-echo "-> local-release exec-cgi"
+echo "-> release exec-cgi"
 
 cmdX(){
 	if ! "$@" ; then
-		echo "Error: local-release"
+		echo "Error: release"
 		exit 1
 	fi
 }
