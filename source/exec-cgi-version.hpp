1a64248bed8e0bf22504595310641dbf4cd3698e
Grigore Stefan <g_stefan@yahoo.com>
2021-07-06 20:19:37 +0300

    update

--- source/exec-cgi-version.hpp
+++ source/exec-cgi-version.hpp
@@ -10,10 +10,10 @@
 #ifndef EXEC_CGI_VERSION_HPP
 #define EXEC_CGI_VERSION_HPP
 
-#define EXEC_CGI_VERSION_ABCD                1,6,0,8
-#define EXEC_CGI_VERSION_STR                 "1.6.0"
-#define EXEC_CGI_VERSION_STR_BUILD           "8"
-#define EXEC_CGI_VERSION_STR_DATETIME        "2021-03-15 12:30:21"
+#define EXEC_CGI_VERSION_ABCD                1,7,0,9
+#define EXEC_CGI_VERSION_STR                 "1.7.0"
+#define EXEC_CGI_VERSION_STR_BUILD           "9"
+#define EXEC_CGI_VERSION_STR_DATETIME        "2021-07-06 15:11:46"
 
 #ifndef XYO_RC
 
